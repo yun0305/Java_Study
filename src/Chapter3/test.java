15b15c680d14a0aac8ac2f741fd23070b3a3e282
yun0305 <heuyoon19@gmail.com>
2021-11-15 07:10:43 +0900

    java study

--- src/Chapter3/test.java
+++ src/Chapter3/test.java
@@ -3,11 +3,11 @@ package Chapter3;
 public class test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("pass");
-=======
-		System.out.println("yes git");
->>>>>>> branch 'master' of https://github.com/yun0305/Java_Study.git
+
+	
+
+		System.out.println("hello");
+
 		for(int i = 0; i<=5; i++) {
 		System.out.println(i);
 		}
