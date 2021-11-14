4eb4c4e5928c6a075b731631a4133ea29377c742
yun0305 <heuyoon19@gmail.com>
2021-11-15 07:03:53 +0900

    uj

--- src/Chapter3/test.java
+++ src/Chapter3/test.java
@@ -3,7 +3,7 @@ package Chapter3;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("new pass");
+		System.out.println("pass");
 		for(int i = 0; i<=5; i++) {
 		System.out.println(i);
 		}
