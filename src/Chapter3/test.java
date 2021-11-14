638f855b1a7d3b4e423ef22473d214bd1124ef45
yun0305 <heuyoon19@gmail.com>
2021-11-15 06:56:53 +0900

    java study

--- src/Chapter3/test.java
+++ src/Chapter3/test.java
@@ -3,7 +3,7 @@ package Chapter3;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("fuckin git");
+		System.out.println("no git");
 		for(int i = 0; i<=5; i++) {
 		System.out.println(i);
 		}
