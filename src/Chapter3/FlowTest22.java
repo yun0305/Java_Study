d4e84982186404047262a8ff170b6693076da455
yun0305 <heuyoon19@gmail.com>
2021-11-16 08:23:19 +0900

    java study

--- src/Chapter3/FlowTest22.java
+++ src/Chapter3/FlowTest22.java
@@ -1,9 +1,28 @@
 package Chapter3;
 
+import java.util.Scanner;
+
 public class FlowTest22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String answer = "서울";
+		String inString = "";
+		
+		
+		do {
+			System.out.println("대한민국 수도를 입력하세요");
+			System.out.println("수도를 입력하세요");
+			inString = scan.next();
+			if(answer.equals(inString)) {
+				System.out.println("정답입니다");
+				break;
+			}
+			else {
+				System.out.println("틀렸습니다");
+			}
+			
+		}while(true);
 
 	}
 
