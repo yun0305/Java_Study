a6e288f1323e3e40e3256a4f575b93d8648d88cd
yun0305 <heuyoon19@gmail.com>
2021-12-13 02:35:31 +0900

    java study

---
+++ src/Chapter4/ArrayTest5.java
@@ -0,0 +1,15 @@
+package Chapter4;
+
+public class ArrayTest5 {
+
+	public static void main(String[] args) {
+		String names[] = new String[] {"레이1","레이2","레이3","레이4","레이5"};
+		
+		for(String name : names) {
+			System.out.println(name);
+		}
+		
+
+	}
+
+}
---
+++ src/Chapter4/ArrayTest6.java
@@ -0,0 +1,21 @@
+package Chapter4;
+
+public class ArrayTest6 {
+
+	public static void main(String[] args) {
+		String cities[] = new String[] {"서울","대구","춘천","울산","광주","천안"};
+		String nation[] = new String[] {"대한민국","미국","영국","일본","프랑스"};
+		
+		String newArray[] = new String[cities.length+nation.length];
+		
+		//System.arraycopy(복사할 대상,몆번째 인덱스부터 복사할지,복사한걸 담을 배열,담을 배열의 몆번째 인덱스에 넣을지,인덱스를 몆개 복사할지(첫번째 기준으로 인덱스번호1));
+		
+		System.arraycopy(cities, 2, newArray, 2, 3);
+		
+		for(String newarray:newArray) {
+			System.out.println(newarray);
+			
+		}
+	}
+
+}
