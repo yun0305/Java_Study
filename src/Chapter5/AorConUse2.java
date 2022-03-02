897dfade5b8b01e26da29ebc8bc984df105e3795
yun0305 <heuyoon19@gmail.com>
2022-03-03 01:38:30 +0900

    java study

--- src/Chapter5/AorConUse2.java
+++ src/Chapter5/AorConUse2.java
@@ -3,7 +3,35 @@ package Chapter5;
 public class AorConUse2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Aircon airCon1 = new Aircon();
+		
+		//변수 사용
+		airCon1.color = "white";
+		airCon1.temp = 10;
+		airCon1.price = 10000;
+		
+		//메소드 사용
+		airCon1.uptemp();
+		System.out.println("airCon1.temp = "+airCon1.temp+", airCon1.color = "+airCon1.color+", airCon1.price = "+airCon1.price+"원"+"\n");
+		
+		airCon1.onPower();
+		airCon1.offPower();
+		airCon1.downTemp();
+		airCon1.color = "black";
+		System.out.println("airCon1.temp = "+airCon1.temp+", airCon1.color = "+airCon1.color+", airCon1.price = "+airCon1.price+"원"+"\n");
+		
+		
+		//두번째 객체 생성
+		Aircon airCon2 = new Aircon();
+		
+		System.out.println("aircon2.temp = "+airCon2.temp+", airCon2.color = "+airCon2.color+", airCon2.price = "+airCon2.price+"원"+"\n");
+		
+		
+		//airCon2 변수에 aircon1 변수 참조 값 할당
+		airCon2 = airCon1;
+		System.out.println("aircon2.temp = "+airCon2.temp+", airCon2.color = "+airCon2.color+", airCon2.price = "+airCon2.price+"원"+"\n");
+		
 
 	}
 
