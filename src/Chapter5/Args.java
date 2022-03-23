25693c2179b0b1373bd7753253535dcba2ef4459
yun0305 <heuyoon19@gmail.com>
2022-03-24 02:48:59 +0900

    java study

---
+++ src/Chapter5/Args.java
@@ -0,0 +1,40 @@
+package Chapter5;
+
+public class Args {
+	int x;
+	
+	void add(int x) {//메소드 명이 같더라도 인자 값의 개수가 다르면 다른 변수로 취급한다
+		x = x+50;
+	}
+	void add(Args arg) {
+		
+		arg.x = arg.x+40;
+	}
+	void addnew(Args arg) {
+		arg = new Args();
+		
+	}
+	void add(int[] arr) {
+		arr[0]++;
+		
+	}
+	public static void main(String[] args) {
+			Args arg = new Args();
+			arg.x = 100;
+			int arr[] = new int[1];
+		
+			arg.add(arg.x);
+			System.out.println("arg.x = "+arg.x);
+			
+			arg.add(arg);
+			System.out.println("arg.x = "+arg.x);
+			
+			arg.addnew(arg);
+			System.out.println("arg.x = "+arg.x);
+			
+			arg.add(arr);
+			System.out.println("arr[0] = "+ arr[0]);
+
+	}
+
+}
---
+++ src/Chapter5/VariableArgument.java
@@ -0,0 +1,42 @@
+package Chapter5;
+
+public class VariableArgument {
+	
+	void printinfo(String ... infos) {
+		if(infos.length !=0) {
+			for(int i=0;i<infos.length;i++) {
+				System.out.println(infos[i]);
+			}
+		}
+		else {
+			System.out.println("인자가 없네요");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		VariableArgument vt = new VariableArgument();
+		
+		System.out.print("인자가 없을때 :");
+		vt.printinfo();
+		
+		System.out.println();
+		
+		System.out.print("인자가 한 개일때 :");
+		vt.printinfo("허윤");
+		
+		System.out.println();
+		
+		System.out.print("인자가 두 개일때 :");
+		vt.printinfo("허윤","개발부");
+		
+		System.out.println();
+		
+		System.out.print("인자가 세 세일때 :");
+		vt.printinfo("허윤","개발부","1억");
+		
+		System.out.println();
+		
+	}
+
+}
