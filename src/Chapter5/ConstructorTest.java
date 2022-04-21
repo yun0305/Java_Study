10df795c41f3aa747a20a9a1cb4e1d9fb628e37e
yun0305 <heuyoon19@gmail.com>
2022-04-22 00:37:57 +0900

    java stusy

---
+++ src/Chapter5/ConstructorTest.java
@@ -0,0 +1,32 @@
+package Chapter5;
+class Person() {
+int age;
+String name; 
+int height;
+int weight;
+
+public preson() {
+	
+}
+public person(int age) {
+	
+}
+public person(Stirng name) {
+	
+
+ }
+}
+
+
+public class ConstructorTest {
+
+
+
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
